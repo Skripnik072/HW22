ba47e7cbabda6ce71191d664a7bdad2f683326af
Виктор Скрипник <viktor_ds@mail.ru>
2025-09-13 16:29:46 +0800

    fixed hw24 ver1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
 from . import views
-from catalog.views import home, contacts
-
+from catalog.views import home, contacts, products_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('home/',  home, name='home'),
-    path('contacts/',  contacts, name='contacts')
+    path('home/', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('products_list/', products_list, name='products_list'),
+    path('catalog/<int:pk>/', product_detail, name='product_detail')
     ]
