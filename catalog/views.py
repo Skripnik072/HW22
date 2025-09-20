a9c7b9628f8512713fdc018d12133228c11278df
Виктор Скрипник <viktor_ds@mail.ru>
2025-09-20 10:33:18 +0800

    fixed hw25 begin

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
 from catalog.models import Product
 
+
+class ProductListView(ListView):
+    model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    queryset = Product.objects.all()
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ("name", "description", "photo", "categor", "price")
+    success_url = reverse_lazy('catalog:product_list')
 
 def home(request):
     return render(request, 'home.html')
@@ -10,13 +27,3 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 
-def products_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, 'products_list.html', context)
-
-
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'product_detail.html', context)
